52f2040f30df958fa7a376b3e4877373bdc412fb
Shane McLean <sdmclean3@hotmail.com>
2017-10-13 06:16:19 +0200

    Added a new 'Helloworld' example project to dotSpace.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using dotSpace.Interfaces.Space;
+using dotSpace.Objects.Space;
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SequentialSpace dtu = new SequentialSpace();
+            dtu.Put("Hello world!");
+            ITuple tuple = dtu.Get(typeof(string));
+            Console.WriteLine(tuple);
+            Console.Read();
+        }
+    }
+}
