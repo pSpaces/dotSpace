7d356fee8d3109bd9a5fd97763528e4541ca6be2
Shane McLean <sdmclean3@hotmail.com>
2017-10-13 09:18:44 +0200

    Added nuget package manifest.

--- dotSpace/Properties/AssemblyInfo.cs
+++ dotSpace/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("dotSpace")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Programming with Spaces in .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DTU")]
+[assembly: AssemblyCompany("Shane McLean")]
 [assembly: AssemblyProduct("dotSpace")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
